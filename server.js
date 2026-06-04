78a12b98f910c41823c86450ac5d8023ec4647ab
Vanessa Moraes <vanessamoraeswd@gmail.com>
2026-06-03 23:25:16 -0300

    feat: implement initial crud endpoints for piercings routing

--- server.js
+++ server.js
@@ -3,7 +3,31 @@ const app = express()
 const PORT = 3001
 
 app.get('/', (req, res) => {
-  res.send({message: "Welcome to the Piercings Store!"})
+  res.send({ message: 'Welcome to the Piercings Store!' })
+})
+
+app.get('/piercings', (req, res) => {
+  res.json({ message: 'Listing all the piercings', piercings: [] })
+})
+
+app.post('/piercings', (req, res) => {
+  res.json({
+    message: 'Creating a new piercing',
+    piercing: { id: Date.now(), name: 'Piercing A' }
+  })
+})
+
+app.put('/piercings/:id', (req, res) => {
+  const { id } = req.params
+  res.json({
+    message: 'Updating piercing',
+    piercing: { id: id, name: 'Updated piercing' }
+  })
+})
+
+app.delete('/piercings/:id', (req, res) => {
+  const { id } = req.params
+  res.json({ message: 'Deleting the piercing', piercingId: id })
 })
 
 app.listen(PORT, () => {
