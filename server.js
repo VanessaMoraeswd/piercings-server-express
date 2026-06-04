bfc16c36fd4abc23581cc2e0490332b16c2e452f
Vanessa Moraes <vanessamoraeswd@gmail.com>
2026-06-03 23:02:31 -0300

    feat: setup express server and create initial root route with rest client test

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express()
 const PORT = 3001
 
 app.get('/', (req, res) => {
-  res.send(`Server is running on port ${PORT}`)
+  res.send({message: "Welcome to the Piercings Store!"})
 })
 
 app.listen(PORT, () => {
-  console.log(`\n⚡ [server] Server running on http://localhost:${PORT}\n`)
+  console.log(`\n⚡ Server running on http://localhost:${PORT}\n`)
 })
