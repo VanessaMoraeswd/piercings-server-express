72d9cc79307a5a37cd25f5eb71daa7801a6eb096
Vanessa Moraes <vanessamoraeswd@gmail.com>
2026-06-09 00:40:16 -0300

    feat: implement about route and create about route rest client test

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const PORT = 3001
 
+app.use((req, res, next) => {
+  console.log(`${req.method} - ${req.url}`)
+  next()
+})
+
 app.get('/', (req, res) => {
   res.send({ message: 'Welcome to the Piercings Store!' })
 })
@@ -45,4 +50,8 @@ app.get('dashboard', checkAuth, (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`\n⚡ Server running on http://localhost:${PORT}\n`)
+})
+
+app.get('/about', (req, res) => {
+  res.send({ message: 'Meet the Piercing Store' })
 })
